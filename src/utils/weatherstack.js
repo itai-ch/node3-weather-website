df055b8980d8c0bd7ed75bb0096ad7360285269a
Itai <itai.chitayat@gmail.com>
2020-09-27 22:34:05 +0300

    Init commit

---
+++ src/utils/weatherstack.js
@@ -0,0 +1,17 @@
+const request = require('request')
+const chalk = require('chalk')
+
+const weatherstack = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=ab1c9a0c7242fdea8b2ea88bf7c32fdf&query=' + latitude + ',' + longitude
+    request ({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location')
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ', it is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+        }
+    })
+}
+
+module.exports = weatherstack
