92b32d5059b9b9bd354505dc59cd29522b5e8f0e
Itai <itai.chitayat@gmail.com>
2020-09-29 02:25:04 +0300

    add relative humidity

--- src/utils/weatherstack.js
+++ src/utils/weatherstack.js
@@ -8,7 +8,7 @@ const weatherstack = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ', it is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + ', it is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The relative humidity is ' + body.current.humidity + ' percent.')
         }
     })
 }
